a4087589b0745981a7e47176aa243a6d1097a090
hoang duy <hoangduy1771@gmail.com>
2023-08-10 01:07:04 +0700

    follow instruction, not inventing new shit. invent is for later

--- src/test/java/testcases/LoginTest.java
+++ src/test/java/testcases/LoginTest.java
@@ -4,6 +4,7 @@ package testcases;
 import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Managed;
+import net.thucydides.core.annotations.Title;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
@@ -14,12 +15,24 @@ public class LoginTest extends PageObject {
     @Managed
     WebDriver driver;
 
+    @Title("Execute Serenity Test")
     @Test
     public void doLogin() throws InterruptedException {
         open();
+        System.out.println(getTitle());
 //        driver.get("https://google.com");
-        driver.findElement(By.name("q")).sendKeys("Hello");
+
+//        -- Using serenity framework methods
+//        getDriver().findElement(By.name("q")).sendKeys("Hello");
+//        find(By.xpath("//*[@name='q']")).sendKeys("Hello");
+
+//        -- For CSS selector or xpath you can use $
+//        $("//*[@name='q']").sendKeys("Hello");
+        typeInto($("//*[@name='q']"), "Hello");
+
+
         Thread.sleep(2000);
+
     }
 
 }
